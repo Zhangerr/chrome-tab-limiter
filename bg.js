4d4e25087e463ec70aff2c0734dbd85e60e13253
alex zhang <alexzhangh@gmail.com>
2012-04-26 19:04:52 -0700

    initial commit

--- bg.js
+++ bg.js
@@ -1,61 +1,54 @@
 var tabnumber = 0;
-var maxtabs = 20;
+var maxtabs = 20; //hardcoded default max tabs
 
 var activated = true;
 var fornow = localStorage["check"];
-if(fornow != null) {
-	activated = fornow == 'true';
+
+if(fornow != null) { 
+	activated = fornow == 'true'; //check if the localstorage has an entry for the extension to be enabled
 }
+
 var mx = localStorage["max_tabs"]
+
 if(mx!=null){
-	maxtabs = parseInt(mx);
+	maxtabs = parseInt(mx); //parse int from max tabs in localstorage
 }
-console.log(maxtabs);
- chrome.windows.getAll({"populate":true},function(windows) {
- var sum = 0;
- for(var i = 0; i < windows.length; i++) {
-	sum+=windows[i].tabs.length;
-	}
-	
-tabnumber=sum;
- chrome.browserAction.setBadgeText({"text":tabnumber +""});
- });
+
+chrome.windows.getAll({"populate":true},function(windows) {
+	var sum = 0;
+	for(var i = 0; i < windows.length; i++) {
+		sum+=windows[i].tabs.length;
+	}	
+	tabnumber=sum;
+	chrome.browserAction.setBadgeText({"text":tabnumber + ""}); //lazy string conversion method
+});
 
 chrome.tabs.onCreated.addListener(function(tab) { 
-
- tabnumber++;
- chrome.browserAction.setBadgeText({"text":tabnumber +""});
+	tabnumber++;
+	chrome.browserAction.setBadgeText({"text":tabnumber +""});
  	if(tabnumber > maxtabs && activated) {
-	chrome.tabs.remove(tab.id);
-	var notification = webkitNotifications.createNotification(
-	'icon.png',  // icon url - can be relative
-	'Alert',  // notification title
-	'Tab limit exceeded.'  // notification body text
-	);
-	notification.show();
+		chrome.tabs.remove(tab.id);
+		var notification = webkitNotifications.createNotification(
+		'icon.png',  // icon url - can be relative
+		'Alert',  // notification title
+		'Tab limit exceeded.'  // notification body text
+		);
+		notification.show();
 	}
-
-  /*chrome.tabs.create({
-          'url':'https://chrome.google.com/webstore',
-          'selected':true
-        });*/
- });
-chrome.extension.onConnect.addListener(function(port) {
-
-  port.onMessage.addListener(function(msg) {
-    if(msg.max != null) {
-		maxtabs = parseInt(msg.max);
-		//console.log(typeof maxtabs);
-	}
-	if(msg.check != null) {
-		activated = msg.check;
-	//	console.log(activated);
-	}
-	
-	
-  });
 });
- chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
- tabnumber--;
-  chrome.browserAction.setBadgeText({"text":tabnumber +""});
- });
+ 
+chrome.extension.onConnect.addListener(function(port) { //listen to main page for changes in max tab limit & whether the extension is enabled, a simpler alternative could be reading from localstorage again
+	port.onMessage.addListener(function(msg) {
+		if(msg.max != null) {
+			maxtabs = parseInt(msg.max);
+		}
+		if(msg.check != null) {
+			activated = msg.check;
+		}		
+	});
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+	tabnumber--;
+	chrome.browserAction.setBadgeText({"text":tabnumber +""});
+});
--- popup.js
+++ popup.js
@@ -1,27 +1,24 @@
-//console.log((chrome.extension.getBackgroundPage()));
-
 window.onload = function() {
-var port = chrome.extension.connect({name: "info"});
-	var select2 = document.getElementById("orz");
-	var fornow = localStorage["check"];
+	var port = chrome.extension.connect({name: "info"});
+	var select2 = document.getElementById("orz"); //my pro variable naming (this is the checkbox)
+	var fornow = localStorage["check"]; //spinner
 
 	if(fornow != null) {
 		select2.checked = fornow == 'true';//hacky, stored as string
 		port.postMessage({check : select2.checked});
 	}
+	
 	select2.onchange=function(){
 		localStorage["check"] = select2.checked;
-	port.postMessage({check : select2.checked});
-
+		port.postMessage({check : select2.checked});
 	}
 	
 	var value = localStorage["max_tabs"];	
 	var select = document.getElementById("sp");
-	//console.log(value);
 	
 	if(value !=null) {	
-	select.value=value;
-	port.postMessage({max : select.value});
+		select.value=value;
+		port.postMessage({max : select.value});
 	}
 
 	document.getElementById("sp").oninput=function(){
